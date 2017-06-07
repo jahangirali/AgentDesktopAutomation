bc2868d304f0f5b4b758831e5a3942c27dff3d97
jahangira <jahangira@FCP-RAG-EXT06>
2017-06-07 15:36:31 +0100

    Refactored Tests Page

--- AgentDesktopFramework/FlightSearchPage.cs
+++ AgentDesktopFramework/FlightSearchPage.cs
@@ -1,73 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 
 namespace AgentDesktopFramework
 {
-    [TestFixture]
     public class FlightSearchPage
-
     {
-        public IWebDriver driver;
-        private String url = "https://d1adin01webr01/login?userID=defaultagent&password=12341234&site=agentDesktop";
-        //private String url = "https://www.easyjet.com/en";
-        
-        [SetUp]
-            public void Setup()
+        private IWebDriver Driver { get; set; }
+
+        public FlightSearchPage(IWebDriver driver)
         {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
+            Driver = driver;
         }
 
-        [Test]
-        public void Test1()
+        public void ExitButton()
         {
-            var loginField = driver.FindElement(By.Id("j_username"));
+            var exitButton = Driver.FindElement(By.Id("close-drawer-link"));
+            exitButton.Click();
+        }
+        public void LoginField()
+        {
+            var loginField = Driver.FindElement(By.Id("j_username"));
             loginField.Click();
-            loginField.SendKeys("rachel");
-            
-            var passwordField = driver.FindElement(By.Id("j_password"));
+        }
+
+        public void PasswordField()
+        {
+            var passwordField = Driver.FindElement(By.Id("j_password"));
             passwordField.Click();
             passwordField.SendKeys("12341234");
-
-            var loginbutton = driver.FindElement(By.CssSelector("button[type*='submit']"));
-            loginbutton.Click();
-
-            Thread.Sleep(5);
-
-            var flightSearchLink = driver.FindElement(By.Id("flightSearchLink"));
-            flightSearchLink.Click();
-
-            var fromAirportField = driver.FindElement(By.Id("from-airport"));
-            fromAirportField.Click();
-            fromAirportField.SendKeys("LGW"+Keys.Tab);
-
-            var toAirportField = driver.FindElement(By.Id("to-airport"));
-            toAirportField.Click();
-            toAirportField.SendKeys("AMS"+Keys.Tab);
-
-            var fromDateField = driver.FindElement(By.Id("from-date"));
-            fromDateField.Click();
-
-            var noOfAdult = driver.FindElement(By.Id("adults-a"));
-            noOfAdult.Click();
-            noOfAdult.SendKeys("3"+Keys.Tab);
-
-            var searchButton = driver.FindElement(By.Id("flight-search-button"));
-            searchButton.Click();
-
-            Thread.Sleep(TimeSpan.FromSeconds(5));  
-              
-            var OKButton = driver.FindElement(By.CssSelector("#erorr-modal-type-3 button[class$='btn-accept']"));
-            OKButton.Click();
-
         }
     }
 }
+   
