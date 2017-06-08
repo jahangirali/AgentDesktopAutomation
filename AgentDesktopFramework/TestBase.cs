04abef3baacca40793a050529e45fb0d4b5a1c74
jahangira <jahangira@FCP-RAG-EXT06>
2017-06-08 14:50:52 +0100

    updated

--- AgentDesktopFramework/TestBase.cs
+++ AgentDesktopFramework/TestBase.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace AgentDesktopFramework
 {
@@ -23,5 +26,75 @@ namespace AgentDesktopFramework
             "sc.Status"
         };
 
+        [OneTimeSetUp]
+        public virtual void OneTimeSetup()
+        {
+            //SelectBrowser();
+            WebDriver = new ChromeDriver();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDriver.Navigate().GoToUrl(SeleniumSettings.SystemUnderTest);
+            DeleteTempCookies();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DeleteTempCookies();
+        }
+
+        [OneTimeTearDown]
+        public virtual void OneTimeTearDown()
+        {
+            DeleteAllCookies();
+            WebDriver?.Quit();
+        }
+
+        private void SelectBrowser()
+        {
+            DesiredCapabilities caps = null;
+            switch "chrome":
+                caps = BrowserCapabilities.GetChromeSettings;
+            break;
+            case "firefox":
+            caps = BrowserCapabilities.GetFirefoxSettings;
+            break;
+            case "internetexplorer":
+            caps = BrowserCapabilities.GetInternetExplorerSettings;
+            break;
+        }
+        WebDriver = new RemoteWebDriver(SeleniumSettings.GridWebDriverUrl, caps, TimeSpan.FromSeconds(150));
+        WebDriver.Manage().Window.Maximize();
+
+        private void DeleteAllCookies()
+        {
+            try
+            {
+                if (WebDriver.Manage().Cookies.AllCookies.Count != 0)
+                {
+                    WebDriver.Manage().Cookies.DeleteAllCookies();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private void DeleteTempCookies()
+        {
+            var cookies = WebDriver.Manage().Cookies.AllCookies;
+            foreach (var cookie in cookies)
+            {
+                if (!RequiredCookies.Contains(cookie.Name))
+                {
+                    WebDriver.Manage().Cookies.DeleteCookie(cookie);
+                }
+            }
+        }
     }
 }
---
+++ Selenium.Settings/BrowserCapabilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Settings
+{
+    public static class BrowserCapabilities
+    {
+        public static DesiredCapabilities GetChromeSettings
+        {
+            get
+            {
+                var options = new ChromeOptions():
+                options.AddArguments("--test-type");
+                options.AddArguments("--disable-extensions");
+                return options.ToCapabilities() as DisiredCapabilities;
+            }
+        }
+    }
+}
